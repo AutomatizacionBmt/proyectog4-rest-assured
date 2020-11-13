e13576774b2ecb1c105dde04f8277beb44bbf634
Jose Hurtado <josehumberto17@gmail.com>
2020-11-13 08:47:50 -0500

    Remove imports

--- src/test/java/com/company/features/RedmineOtherTest.java
+++ src/test/java/com/company/features/RedmineOtherTest.java
@@ -12,9 +12,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 import static io.restassured.RestAssured.given;
