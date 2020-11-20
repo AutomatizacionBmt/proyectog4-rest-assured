df1c275a65a77a415ef0bddade1e013043dbb3ff
Jose Hurtado <josehumberto17@gmail.com>
2020-11-19 21:32:12 -0500

    Agregar nuevo scenario para issue

--- src/test/java/com/company/cucumber/stepdefs/RestIssuesStepDef.java
+++ src/test/java/com/company/cucumber/stepdefs/RestIssuesStepDef.java
@@ -1,11 +1,16 @@
 package com.company.cucumber.stepdefs;
 
 import com.company.util.RedmineEndpoints;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.junit.Assert;
+
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 
@@ -32,5 +37,36 @@ public class RestIssuesStepDef {
                 response.
                         then()
                         .statusCode(statusCode);
+    }
+
+    @When("System send a request to get issues service by id")
+    public void systemSendARequestToGetIssuesServiceById(Map<String, String> issue) {
+
+        response = request.
+                pathParam("idIssue", issue.get("id")).
+        when()
+                .get(RedmineEndpoints.SINGLE_REDMINE_ISSUES_JSON);
+    }
+
+    @And("System should responds with response data")
+    public void systemShouldRespondsWithResponseData(Map<String, String> expectedData) {
+
+        JsonPath actualData = new JsonPath(response.getBody().asString());
+
+        Assert.assertEquals("El id no es el correcto",
+                Integer.parseInt(expectedData.get("id")),
+                actualData.getInt("issue.id"));
+
+        Assert.assertEquals("El subject no es correcto",
+                expectedData.get("subject"),
+                actualData.getString("issue.subject"));
+
+        Assert.assertEquals("La descripcion no es correcta",
+                expectedData.get("description"),
+                actualData.getString("issue.description"));
+
+        Assert.assertEquals("La fecha no es correcta",
+                expectedData.get("start_date"),
+                actualData.getString("issue.start_date"));
     }
 }
--- src/test/resources/features/rest-issues.feature
+++ src/test/resources/features/rest-issues.feature
@@ -12,8 +12,11 @@ Feature: Rest - Redmine Testing - Issues
   Scenario: Get issues by id - JSON
     Given System is ready to send requests
     When System send a request to get issues service by id
-    |id|840|
+      | id | 840 |
     Then The response status should be 200
     And System should responds with response data
-
+      | id          | 840                                                   |
+      | subject     | I cannot create a user.                               |
+      | description | As an admin user, I cannot create an user when xml... |
+      | start_date  | 2020-08-15                                            |
 
